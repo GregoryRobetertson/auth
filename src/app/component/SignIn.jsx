965a6488b1ffbbe5b26ab173f71fa86cf7a5ec08
GregoryRobetertson <geefromla@gmail.com>
2024-05-01 11:11:12 -0500

    Created logic to sign upusing email and password

--- src/app/component/SignIn.jsx
+++ src/app/component/SignIn.jsx
@@ -1,19 +1,35 @@
 'use client';
-import React from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react'
+import { auth } from '../firebaseConfig';
 
 export default function SignIn() {
+  const[ userObj, setUser] = useState(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  async function handleSignUpWithEmail(e) {
+    e.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      console.log('User created:', userCredential.user);
+      setUser(userCredential.user)
+    } catch (error) {
+      console.error('error signing up', error)
+    }
+
+  }
   return (
     <>
-    <form>
+    <form onSubmit={handleSignUpWithEmail}>
         <label>
             Email
-            <input type="email" name="email" id="email" placeholder='Email' />
+            <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" placeholder='Email' />
         </label>
         <label>
             Password 
-            <input type="password" name="password" id="password" placeholder='Password' />
+            <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder='Password' />
         </label>
-        <button type="submit">Login</button>
+        <button type="submit">Sign Up</button>
     </form>
     </>
   )
