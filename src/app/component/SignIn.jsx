fcc263d563cf69d19635d3de1827ff054946e904
GregoryRobetertson <geefromla@gmail.com>
2024-05-01 16:08:03 -0500

    Created function to sign in with google

--- src/app/component/SignIn.jsx
+++ src/app/component/SignIn.jsx
@@ -1,23 +1,37 @@
 'use client';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import React, { useState } from 'react'
-import { auth } from '../firebaseConfig';
+import { auth, provider } from '../firebaseConfig';
 
 export default function SignIn() {
-  const[ userObj, setUser] = useState(null);
+  const[ userObj, setUserObj] = useState(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  // Sign up with email and password 
   async function handleSignUpWithEmail(e) {
     e.preventDefault();
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       console.log('User created:', userCredential.user);
-      setUser(userCredential.user)
+      setUserObj(userCredential.user)
     } catch (error) {
       console.error('error signing up', error)
     }
+}
 
+// Sign in with Google
+const handleGoogleSignIn = async () => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+    setUserObj(user);
+    console.log('user signed in:', user.displayName );
+  } catch (error) {
+    console.error('Error signing in:', error);
   }
+}
+    
   return (
     <>
     <form onSubmit={handleSignUpWithEmail}>
@@ -31,6 +45,8 @@ export default function SignIn() {
         </label>
         <button type="submit">Sign Up</button>
     </form>
+
+    {!userObj && ( <button onClick={handleGoogleSignIn}>Sign in with Google</button>)}
     </>
   )
 }
