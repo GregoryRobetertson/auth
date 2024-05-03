79a793680c5bce0a5e4d66150657273f98e1a0da
GregoryRobetertson <geefromla@gmail.com>
2024-05-02 22:12:42 -0500

    Updated page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
-
-import SignIn from "./component/SignIn";
-
+'use client';
+import { onAuthStateChanged } from "firebase/auth";
+import SignIn from "./SignIn";
+import { useRouter } from 'next/navigation';
+import { auth } from "./firebaseConfig";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+  const unsubscribe =onAuthStateChanged(auth, (user) => {
+    if(user){
+      router.push('/dashboard');
+    } else{
+      router.push('/');
+    }
+  })
+  return () => unsubscribe();
+  }, []);
+  
+  const router = useRouter();
   return (
     <main>
+      <h1>Welcome to my site!</h1>
       <SignIn/>
     </main>
   );
